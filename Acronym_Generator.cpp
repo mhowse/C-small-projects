bc6ec92758f74fafa83153e80320f1361101e36b
mhowse <mikhaila.howse@gmail.com>
2016-04-01 22:20:06 +1300

    Start of an acronym generator in c++, heavily based off the java version.

---
+++ Acronym_Generator.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   Acronym_Generator.cpp
+ * Author: mhowse;
+ *
+ * Created on 1 April 2016, 9:59 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    string data= std::cin;
+    return 0;
+}
+
