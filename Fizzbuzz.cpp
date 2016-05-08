7ff66b0e5fda1d8035354dc787aa886dbab0b5b9
mhowse <mikhaila.howse@gmail.com>
2016-05-08 21:27:24 +1200

    FizzBuzz finished.

--- Fizzbuzz.cpp
+++ Fizzbuzz.cpp
@@ -15,16 +15,22 @@ using namespace std;
 int main (){
 
 for(int i=1; i<101; i++){
-if( (i%3)==0){
-cout<<"Fizz";
-}
-if( (i%5)==0){
-cout<<"Buzz";
-}
+
+if((i%15)==0)
+cout<<"FizzBuzz\n";
+
+else  if( (i%3)==0)
+cout<<"Fizz\n";
+
+else if( (i%5)==0)
+cout<<"Buzz\n";
+
+
 else
 cout<<i<<"\n";
 
-}
+
+}/*end for loop*/
 
 return 0;
 }
