262fd37eba2fe10fc7cb56c41ff53cc2bfbc85db
mhowse <mikhaila.howse@gmail.com>
2016-06-12 15:09:22 +1200

    start of a c++ pomodoro timer program

---
+++ pomodoroTimer.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   pomodoroTimer.cpp
+ * Author: cleddyf
+ *
+ * Created on 12 June 2016, 3:08 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
